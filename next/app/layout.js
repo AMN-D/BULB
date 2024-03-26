fbe25e804d3e717cccb36864c5e00f3e2ada1ae8
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-03-26 22:06:36 +0530

    FIGURING OUT LAYOUT

--- next/app/layout.js
+++ next/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black flex min-h-screen flex-col items-center justify-between p-24 bg-black`}>
+      <body className={`${inter.className} bg-black flex min-h-screen flex-col items-center justify-between p-16 bg-black`}>
         <div >
           <h1 className=" text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
