9198e20b7801845a8f6d25dce2388542bd163ea8
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-03-27 22:31:27 +0530

    Update main.js

--- javascript testing/main.js
+++ javascript testing/main.js
@@ -1,13 +1,13 @@
 var body = document.querySelector("body");
-body.style.cssText = "font-family: Poppins, sans-serif; background-color: #121212;";
+body.style.cssText = "margin: 0; font-family: Poppins, sans-serif; background-color: #121212;";
 
 var main = document.createElement("main");
-main.style.cssText = "padding: 2rem 6rem; display: flex; flex-direction: column; justify-content: center; align-items: center;";
+main.style.cssText = "padding: 1rem 4rem; display: flex; overflow: hidden; flex-direction: column; justify-content: center; align-items: center;";
 body.appendChild(main);
 
 var heading = document.createElement("h1");
 heading.textContent = "Light Bulb"
-heading.style.cssText = "color: beige; width:100%; font-size: 4rem; text-align: center; transition: all 0.1s ease-in-out; ";
+heading.style.cssText = "color: beige; font-size: 4rem; text-align: center; transition: all 0.15s ease-in-out; ";
 heading.addEventListener("mouseover", () => {
   heading.style.color = "#F4CA56";
 })
@@ -15,3 +15,6 @@ heading.addEventListener("mouseout", () => {
   heading.style.color = "beige";
 })
 main.appendChild(heading);
+
+// bulb
+
